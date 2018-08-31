2245b2c7b615975fa59b23b0f8b12826cdf55bb0
MaximeDeWolf <MaximeDeWolf@outlook.fr>
2018-08-31 19:49:20 +0200

    Setup for future testing

---
+++ code/src/test/concat_test.py
@@ -0,0 +1,17 @@
+import unittest
+
+
+class TestConcatItems(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def testtest(self):
+        self.assertTrue(True)
+
+
+if __name__ == '__main__':
+    unittest.main()
